17317be6e58a4673ecbce5eecac1d181881cae3e
Seth J. Williams <sjwilliams3@students.unwsp.edu>
2024-12-06 23:03:37 -0600

    Update Williams_Wk13_Python_Program4.py

--- Williams_Wk13_Python_Program4.py
+++ Williams_Wk13_Python_Program4.py
@@ -1,5 +1,4 @@
 import sqlite3
-from random import choice
 
 MIN_CHOICE = 1
 MAX_CHOICE = 5
@@ -8,6 +7,78 @@ READ = 2
 UPDATE = 3
 DELETE = 4
 EXIT = 5
+
+
+def main():
+    # Connect to the database.
+    conn = sqlite3.connect('phonebook.db')
+
+    # Get a database cursor.
+    cur = conn.cursor()
+
+    # Add the Cities table.
+    add_entries_table(cur)
+
+    # Add rows to the Cities table.
+    add_entries(cur)
+
+    # Commit the changes.
+    conn.commit()
+
+    # Display the cities.
+    display_entries(cur)
+
+    # Close the connection.
+    conn.close()
+
+
+# The add_cities_table adds the Cities table to the database.
+def add_entries_table(cur):
+    # If the table already exists, drop it.
+    cur.execute('DROP TABLE IF EXISTS Entries')
+
+    # Create the table.
+    cur.execute('''CREATE TABLE Entries (CallerID INTEGER PRIMARY KEY NOT NULL,
+                                        CallerName TEXT,
+                                        CallerNumber REAL)''')
+
+
+# The add_cities function adds 20 rows to the Entries table.
+def add_entries(cur):
+    entries = [(1, 'Tokyo', 38001000),
+               (2, 'Delhi', 25703168),
+               (3, 'Shanghai', 23740778),
+               (4, 'Sao Paulo', 21066245),
+               (5, 'Mumbai', 21042538),
+               (6, 'Mexico City', 20998543),
+               (7, 'Beijing', 20383994),
+               (8, 'Osaka', 20237645),
+               (9, 'Cairo', 18771769),
+               (10, 'New York', 18593220),
+               (11, 'Dhaka', 17598228),
+               (12, 'Karachi', 16617644),
+               (13, 'Buenos Aires', 15180176),
+               (14, 'Kolkata', 14864919),
+               (15, 'Istanbul', 14163989),
+               (16, 'Chongqing', 13331579),
+               (17, 'Lagos', 13122829),
+               (18, 'Manila', 12946263),
+               (19, 'Rio de Janeiro', 12902306),
+               (20, 'Guangzhou', 12458130)]
+
+    for entry in entries:
+        cur.execute('''INSERT INTO Entries (CallerID, CallerName, CallerNumber)
+                       VALUES (?, ?, ?)''', (entry[0], entry[1], entry[2]))
+
+
+# The display_cities function displays the contents of
+# the Cities table.
+def display_entries(cur):
+    print('Contents of phonebook.db/Entries table:')
+    cur.execute('SELECT * FROM Entries')
+    results = cur.fetchall()
+    for row in results:
+        print(f'{row[0]:<3}{row[1]:20}{row[2]:,.0f}')
 
 def main():
     choice = 0
@@ -42,8 +113,8 @@ def get_menu_choice():
     return choice
 
 def create():
-    print('Create New Item')
-    name = input('Item Name: ')
+    print('Create New Contact')
+    name = input('Contact Name: ')
     number = input('Phone Number: ')
     insert_row(name, number)
 
@@ -69,7 +140,7 @@ def delete():
 
     sure = input("Are you sure you want to delete this contact? (y/n): ")
     if sure.lower() == 'y':
-        num_deleted = delete_row(selected_id)
+        num_deleted = delete_entry(selected_id)
         print(f'{num_deleted} row(s) deleted.')
 
 def insert_row(name, number):
@@ -77,7 +148,7 @@ def insert_row(name, number):
     try:
         conn = sqlite3.connect('phonebook.db')
         cur = conn.cursor()
-        cur.execute('''INSERT INTO Entries (ItemName, Number)
+        cur.execute('''INSERT INTO Entries (CallerName, CallerNumber)
         VALUES (?, ?)''',
                     (name, number))
         conn.commit()
@@ -94,7 +165,7 @@ def display_item(name):
         conn = sqlite3.connect('phonebook.db')
         cur = conn.cursor()
         cur.execute('''SELECT * FROM Entries
-        WHERE lower(ItemName) == ?''',
+        WHERE lower(CallerName) == ?''',
                     (name.lower(),))
         results = cur.fetchall()
 
@@ -114,7 +185,7 @@ def update_row(id, name, number):
         conn = sqlite3.connect('phonebook.db')
         cur = conn.cursor()
         cur.execute('''UPDATE Entries
-        SET Itemname = ?, Number = ?
+        SET CallerName = ?, CallerNumber = ?
         WHERE CallerID == ?''',
                     (name, number, id))
         conn.commit()
@@ -133,7 +204,7 @@ def delete_row(id):
         conn = sqlite3.connect('phonebook.db')
         cur = conn.cursor()
         cur.execute('''DELETE FROM Entries
-        WHERE ItemID == ?''',
+        WHERE CallerID == ?''',
                     (id,))
         conn.commit()
         num_deleted = cur.rowcount
@@ -144,6 +215,8 @@ def delete_row(id):
             conn.close()
 
     return num_deleted
+
+
 
 if __name__ == '__main__':
     main()
